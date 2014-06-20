f17a026947489585cc838f74620c208d7d182eed
Mario Franco <marioyfranco@gmail.com>
2014-06-19 19:23:27 -0500

    commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Francoal::Application.config.secret_token = '4ad60aaf990f450260212c47ab087752192434adc62eeba14fa1363737b9f0ad83c24075e667739ebbd6a2d6e494a0c2f308578d7407ad0bcfc7d6b16fed0a23'
